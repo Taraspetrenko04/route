1497742edab3735ac76b244e6d963e84319c982d
unknown <Taraspetrenko04@gmail.com>
2020-03-09 12:01:49 +0200

    Updates

---
+++ precache-manifest.b5155cb42d9d97620635c9376dddb9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502526321f3b62afc60b12587f6e9a7a",
+    "url": "//taraspetrenko04.github.io/route/index.html"
+  },
+  {
+    "revision": "9459d2d3a48656625c47",
+    "url": "//taraspetrenko04.github.io/route/static/css/main.c8a2af03.chunk.css"
+  },
+  {
+    "revision": "6fdb6e8744864f586b22",
+    "url": "//taraspetrenko04.github.io/route/static/js/2.49d272a2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "//taraspetrenko04.github.io/route/static/js/2.49d272a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9459d2d3a48656625c47",
+    "url": "//taraspetrenko04.github.io/route/static/js/main.629acdc7.chunk.js"
+  },
+  {
+    "revision": "9fa5ff23c6c462d7eb19",
+    "url": "//taraspetrenko04.github.io/route/static/js/runtime-main.60f49c25.js"
+  }
+]);
