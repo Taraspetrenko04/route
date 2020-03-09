9bfe12fb24b0554a545a95cef48c5dcef632e101
unknown <Taraspetrenko04@gmail.com>
2020-03-09 17:26:23 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Layout from './components/Layout/Layout';
 import Store from './components/Store/Store';
 import Slider from './components/Slider/Slider';
 
-function App() {
+class App extends Component {
+  
+  render(){
   return (
     <React.Fragment>
       <Header />
-      <Switch>
-        <Route path='/' exact component={Store}/>
-        <Route path='/layout' component={Layout}/>
-        <Route path='/slider' component={Slider}/>
+      <Switch >
+        <Route path='/' exact render={ () => <Store />}/>
+        <Route path='/layout' render={ () => <Layout />}/>
+        <Route path='/slider' render={ () => <Slider />}/>
       </Switch>
       <Footer />
     </React.Fragment>
   );
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './style.css';
+import { Redirect } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 export default class Header extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     // this.state = {isToggleOn: true};
+    
+    //     // Эта привязка обязательна для работы `this` в колбэке.
+    //     this.handleClick = this.handleClick.bind(this);
+    //     this.handleClick = this.handleClick.bind(this);
+    //   }
+
+      handleClick(e) {
+       console.log(`/${e.target.name}`);
+       return <Redirect to={`/${e.target.name}`} />
+      }
+    
     render(){
     return(
         <div className='header'>
             <h1>Header</h1>
+            <ul>
+            <li><Link to="/">STORE</Link></li>
+            <li><Link to="/layout">LAYOUT</Link></li>
+            <li><Link to="/slider">SLIDER</Link></li>
+        </ul>
         </div>
     )}
 }
+
